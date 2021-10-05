3e57a6303924cb454e712ac95a3602940806ccee
woo00oo <96woo94@naver.com>
2021-10-05 22:10:00 +0900

    엔티티 클래스 개발 - member, Order, OrderItem, Item(Movie, Book, Album), Delivery

---
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,25 @@
+package jpabook.jpashop.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 상속관계 매핑
+@DiscriminatorColumn(name = "dtype")
+@Getter @Setter
+public abstract class Item {
+
+    @Id @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+
+    private int price;
+
+    private int stockQuantity;
+
+
+}
