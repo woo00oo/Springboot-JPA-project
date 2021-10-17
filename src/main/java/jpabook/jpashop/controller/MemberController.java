1919e9051ed5795ead2cc144c50ba41bf9f73ee5
woo00oo <96woo94@naver.com>
2021-10-17 21:53:13 +0900

    회원 목록 조회 개발

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,13 +19,13 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    @GetMapping("/members/new")
+    @GetMapping(value = "/members/new")
     public String createForm(Model model){
-        model.addAttribute("memberFrom", new MemberForm());
+        model.addAttribute("memberForm", new MemberForm());
         return "members/createMemberForm";
     }
 
-    @PostMapping("/members/new")
+    @PostMapping(value = "/members/new")
     public String create(@Valid MemberForm form, BindingResult result){
 
         if (result.hasErrors()){
@@ -40,5 +41,12 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
