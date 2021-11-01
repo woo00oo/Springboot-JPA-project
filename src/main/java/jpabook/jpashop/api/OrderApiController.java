86c004092d3a32d623984c7eee39709401a807e7
woo00oo <96woo94@naver.com>
2021-11-01 22:14:28 +0900

    주문 조회 v3 : 엔티티를 DTO로 변환 - 페이징과 한계 돌파

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -9,6 +9,7 @@ import jpabook.jpashop.repository.OrderSearch;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -68,6 +69,18 @@ public class OrderApiController {
     @GetMapping("/api/v3/orders")
     public List<OrderDto> ordersV3(){
         List<Order> orders = orderRepository.findAllWithItem();
+        return orders.stream()
+                .map(OrderDto::new)
+                .collect(toList());
+    }
+
+    //xxxToOne 관계는 페치 조인으로 쿼리수를 줄이고, 나머지는 hibernate.default_batch_fetch_size 로 최적화!
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> ordersV3_page(
+            @RequestParam(value = "offset", defaultValue = "0") int offset,
+            @RequestParam(value = "limit", defaultValue = "100") int limit
+    ){
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
         return orders.stream()
                 .map(OrderDto::new)
                 .collect(toList());
